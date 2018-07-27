6d2b35f5159be344c2e64524613aa2dba4992c6b
Surjith S M <surjithctly@gmail.com>
2018-07-27 14:35:59 +0530

    Update and rename notification.js to notification.json

--- notification.js
+++
@@ -1,2 +0,0 @@
-var auth_notification_id = '1';
-var auth_notification_content = 'Join our Un-offical Slack Community for Envato Authors <a href="http://envato-authors.com/" target="_blank">Join Now</a>';
